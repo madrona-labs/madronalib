4bc31911bfcaefb31e0e920faf7546a328f84979
John Matter <jmatter4@gmail.com>
2025-12-10 15:50:14 -0500

    create FileLogger
    optionally a sink for ActorLogger

--- source/app/MLActorLogger.cpp
+++ source/app/MLActorLogger.cpp
@@ -3,6 +3,9 @@
 // Distributed under the MIT license: http://madrona-labs.mit-license.org/
 
 #include "MLActorLogger.h"
+
+#include <sstream>
+
 #include "MLOSCBuilder.h"
 
 namespace ml
@@ -44,6 +47,24 @@ void ActorLogger::disable()
   }
 }
 
+void ActorLogger::enableFileOutput(bool enable)
+{
+  fileOutputEnabled_ = enable;
+  if (enable)
+  {
+    // Ensure the file logger is enabled
+    getFileLogger().enable();
+
+    // If OSC logging isn't enabled, we still need to set up the callback
+    if (!enabled_)
+    {
+      Actor::setLogCallback([this](Path actorName, const Message& m, bool isEnqueue) {
+        handleLogCallback(actorName, m, isEnqueue);
+      });
+    }
+  }
+}
+
 void ActorLogger::handleLogCallback(Path actorName, const Message& m, bool isEnqueue)
 {
   // Skip anonymous (unregistered) actors
@@ -68,21 +89,38 @@ void ActorLogger::handleLogCallback(Path actorName, const Message& m, bool isEnq
     return;
   }
 
-  // Build OSC address: /log/<enqueue|dispatch>/<actor_name>
-  TextFragment prefix = isEnqueue ? TextFragment("log/enqueue/") : TextFragment("log/dispatch/");
-  TextFragment actorText = actorName.toText();
-  TextFragment addressText(prefix, actorText);
-  Path logAddress = runtimePath(addressText);
+  // OSC output (when OSC logging is enabled)
+  if (enabled_)
+  {
+    // Build OSC address: /log/<enqueue|dispatch>/<actor_name>
+    TextFragment prefix = isEnqueue ? TextFragment("log/enqueue/") : TextFragment("log/dispatch/");
+    TextFragment actorText = actorName.toText();
+    TextFragment addressText(prefix, actorText);
+    Path logAddress = runtimePath(addressText);
 
-  // Send OSC message with:
-  // 1. Message address (string)
-  // 2. Message value (varies)
-  // 3. Message flags (int)
-  OSCMessageBuilder msg(logAddress);
-  msg.addString(m.address.toText());
-  msg.add(m.value);
-  msg.addInt(static_cast<int>(m.flags));
-  msg.sendTo(sender_);
+    // Send OSC message with:
+    // 1. Message address (string)
+    // 2. Message value (varies)
+    // 3. Message flags (int)
+    OSCMessageBuilder msg(logAddress);
+    msg.addString(m.address.toText());
+    msg.add(m.value);
+    msg.addInt(static_cast<int>(m.flags));
+    msg.sendTo(sender_);
+  }
+
+  // File output (when file logging is enabled)
+  if (fileOutputEnabled_)
+  {
+    std::ostringstream oss;
+    oss << (isEnqueue ? "enqueue" : "dispatch") << " " << actorName.toText().getText() << " -> "
+        << m.address.toText().getText() << " = " << m.value;
+    if (m.flags != 0)
+    {
+      oss << " (flags: " << static_cast<int>(m.flags) << ")";
+    }
+    getFileLogger().debug("Actor", oss.str().c_str());
+  }
 }
 
 }  // namespace ml
