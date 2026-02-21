9e48594988fdc029ad726ec3b07aec7192e0bf59
Randy Jones <randy@madronalabs.com>
2026-02-20 17:59:28 -0800

    pass GenericPath by const reference in free functions

--- source/app/MLPath.h
+++ source/app/MLPath.h
@@ -184,49 +184,49 @@ protected:
 
 
 template <class K>
-inline K head(GenericPath<K> p)
+inline K head(const GenericPath<K>& p)
 {
   return p.getSize() > 0 ? p.getElement(0) : K();
 }
 
 template <class K>
-inline K first(GenericPath<K> p)
+inline K first(const GenericPath<K>& p)
 {
   return head(p);
 }
 
 template <class K>
-inline K second(GenericPath<K> p)
+inline K second(const GenericPath<K>& p)
 {
   return p.getSize() > 1 ? p.getElement(1) : K();
 }
 
 template <class K>
-inline K third(GenericPath<K> p)
+inline K third(const GenericPath<K>& p)
 {
   return p.getSize() > 2 ? p.getElement(2) : K();
 }
 
 template <class K>
-inline K fourth(GenericPath<K> p)
+inline K fourth(const GenericPath<K>& p)
 {
   return p.getSize() > 3 ? p.getElement(3) : K();
 }
 
 template <class K>
-inline K fifth(GenericPath<K> p)
+inline K fifth(const GenericPath<K>& p)
 {
   return p.getSize() > 4 ? p.getElement(4) : K();
 }
 
 template <class K>
-inline K nth(GenericPath<K> p, size_t n)
+inline K nth(const GenericPath<K>& p, size_t n)
 {
   return p.getSize() > n ? p.getElement(n) : K();
 }
 
 template <class K>
-inline GenericPath<K> tail(GenericPath<K> p)
+inline GenericPath<K> tail(const GenericPath<K>& p)
 {
   GenericPath<K> r;
   for (int n = 1; n < p.getSize(); ++n)
@@ -237,7 +237,7 @@ inline GenericPath<K> tail(GenericPath<K> p)
 }
 
 template <class K>
-inline GenericPath<K> butLast(GenericPath<K> p)
+inline GenericPath<K> butLast(const GenericPath<K>& p)
 {
   GenericPath<K> r;
   for (int n = 0; n < p.getSize() - 1; ++n)
@@ -248,13 +248,13 @@ inline GenericPath<K> butLast(GenericPath<K> p)
 }
 
 template <class K>
-inline K last(GenericPath<K> p)
+inline K last(const GenericPath<K>& p)
 {
   return p.getSize() > 0 ? p.getElement(p.getSize() - 1) : K();
 }
 
 template <class K>
-inline GenericPath<K> lastN(GenericPath<K> p, size_t n)
+inline GenericPath<K> lastN(const GenericPath<K>& p, size_t n)
 {
   auto len = p.getSize();
   if (len >= n)
@@ -331,7 +331,7 @@ inline Path::GenericPath(const TextFragment& frag)
 }
 
 // GenericPath<Symbol>-specific helper functions
-inline Path substitute(Path p, Symbol from, Symbol to)
+inline Path substitute(const Path& p, Symbol from, Symbol to)
 {
   Path r{p};
   for (int n = 0; n < p.getSize(); ++n)
@@ -344,7 +344,7 @@ inline Path substitute(Path p, Symbol from, Symbol to)
   return r;
 }
 
-inline Path substitute(Path p, Symbol fromSym, Path toPath)
+inline Path substitute(const Path& p, Symbol fromSym, const Path& toPath)
 {
   Path r;
   for (int n = 0; n < p.getSize(); ++n)
