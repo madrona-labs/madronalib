7cba0aad48828c011d4a78da6687a466effc001d
Randy Jones <randy@madronalabs.com>
2026-02-20 20:25:00 -0800

    fix stuck notes when host buffer size is smaller than DSP vector size

--- source/app/MLSignalProcessBuffer.h
+++ source/app/MLSignalProcessBuffer.h
@@ -26,9 +26,15 @@ class SignalProcessBuffer final
   // max chunk size for outside I/O
   size_t maxFrames_;
 
+  // samples accumulated since the last processVector call.
+  // used to remap event times from host-buffer-relative to internal timeline.
+  int samplesAccumulated_{0};
+
  public:
   SignalProcessBuffer(size_t inputs, size_t outputs, size_t maxFrames);
   ~SignalProcessBuffer();
+
+  int getSamplesAccumulated() const { return samplesAccumulated_; }
 
   void process(const float** inputs, float** outputs, int nFrames, AudioContext* ctx,
                SignalProcessFn processFn, void* pState);
