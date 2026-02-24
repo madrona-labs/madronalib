d277262a085d3446354316bf0f5cd1b862b875dd
John Matter <jmatter4@gmail.com>
2026-02-23 22:49:42 -0500

    Update DSP ops tutorial for DSPVector -> SignalBlock rename

--- examples/tutorial/dspOpsExample.cpp
+++ examples/tutorial/dspOpsExample.cpp
@@ -12,8 +12,8 @@
 #endif
 
 using namespace ml;
- 
-constexpr float mySinFillFn(int n){ return const_math::sin(n*kTwoPi/(kFloatsPerDSPVector));  }
+
+constexpr float mySinFillFn(int n){ return const_math::sin(n*kTwoPi/(kFramesPerBlock));  }
 	
 int main()
 {
@@ -26,7 +26,7 @@ int main()
 	std::cout << "DSP Ops:\n";
 	
  	// columnIndex()
-	DSPVector ci = columnIndex();
+	SignalBlock ci = columnIndex();
 	std::cout << "index: " << ci << "\n\n";
 
 	// generate a vector using map() and columnIndex()
@@ -34,36 +34,36 @@ int main()
 
 	auto sinMadronaLib = sin(rangeOpen(0, kTwoPi));
 	std::cout << "madronalib sin: " << sinMadronaLib << "\n\n";
-	 	
-	// store a lambda on (DSPVector)->(DSPVector) defined using map(float)->(float)
-	auto sinNative = [&](const DSPVector& x){ return map( [](float x){ return sinf(x*kTwoPi/(kFloatsPerDSPVector)); }, x); }(columnIndex());
-	std::cout << "native sin: " << sinNative << "\n\n";	
-	
+
+	// store a lambda on (SignalBlock)->(SignalBlock) defined using map(float)->(float)
+	auto sinNative = [&](const SignalBlock& x){ return map( [](float x){ return sinf(x*kTwoPi/(kFramesPerBlock)); }, x); }(columnIndex());
+	std::cout << "native sin: " << sinNative << "\n\n";
+
 	std::cout << "difference from native: " << sinNative - sinMadronaLib << "\n\n";
-	
+
 	// constexpr fill. unfortunately this cannot be made to work with a lambda in C++11.
-	ConstDSPVector kSinVec(mySinFillFn);
+	constexpr SignalBlock kSinVec(mySinFillFn);
 	std::cout << "constexpr sin table: " << kSinVec << "\n\n";
-	
+
 	std::cout << "difference from native: " << sinNative - kSinVec << "\n\n";
 
-	DSPVectorInt iv1(23);
+	SignalBlockInt iv1(23);
 	std::cout << "int fill: " << iv1 << "\n\n";
 
-	DSPVectorInt iv2(truncateFloatToInt(columnIndex()));
+	SignalBlockInt iv2(truncateFloatToInt(columnIndex()));
 	std::cout << "int index: " << iv2 << "\n\n";
 
-	NoiseGen r;
-	DSPVectorInt iv3(truncateFloatToInt(r()*DSPVector(64)));
+	NoiseGen<float> r;
+	SignalBlockInt iv3(truncateFloatToInt(r()*SignalBlock(64)));
 	std::cout << "rand ints in [-64, 64]: " << iv3 << "\n\n";
 	
 	// for filters example / test
 	FDN<4> f;
-	// NOTE: the minimum possible delay time is kFloatsPerDSPVector.
-	f.setDelaysInSamples({{67, 73, 91, 103}}); 
+	// NOTE: the minimum possible delay time is kFramesPerBlock.
+	f.setDelaysInSamples({{67, 73, 91, 103}});
 	f.setFilterCutoffs({{0.1f, 0.2f, 0.3f, 0.4f}});
 	f.mFeedbackGains = {{0.5f, 0.5f, 0.5f, 0.5f}};
-	DSPVector silence, impulse;
+	SignalBlock silence, impulse;
 	impulse[0] = 1.f;
 	std::cout << "silence:" << silence << "\n";
 	std::cout << "impulse:" << impulse << "\n";
@@ -71,73 +71,68 @@ int main()
 	std::cout << f(silence) << "\n";
 	std::cout << f(silence) << "\n";
 	
-	OnePole op;
-	op.mCoeffs = OnePole::coeffs(0.15f);
+	OnePole<float> op;
+	op.coeffs = OnePole<float>::makeCoeffs({0.15f});
 	std::cout << op(impulse) << "\n";
 	std::cout << op(silence) << "\n";
 	std::cout << op(silence) << "\n";
-	
-	// clear filter 
-	op = OnePole();
+
+	// clear filter
+	op = OnePole<float>();
 	// restore coeffs
-	op.mCoeffs = OnePole::coeffs(0.15f);
+	op.coeffs = OnePole<float>::makeCoeffs({0.15f});
 	std::cout << op(silence) << "\n";
 	std::cout << op(impulse) << "\n";
 	
 	
-	TickGen ticks(16);
+	TickGen<float> ticks(16);
 	
 
-	Lopass lp1;
-	lp1.mCoeffs = Lopass::coeffs(0.25, 1.0);
+	Lopass<float> lp1;
+	lp1.coeffs = Lopass<float>::makeCoeffs({0.25f, 1.0f});
 	
 	
-	DSPVector tick;
+	SignalBlock tick;
 	tick[0] = 1;
-	
+
 
 	// upsampler for a generator with 1 input row
-  TestSineGen sineGen;
+  TestSineGen<float> sineGen;
 	Upsample2xFunction<1> upper;
-	std::cout << "\n\n" << upper([&](const DSPVector x) { return sineGen(x); }, DSPVector(440.f / 44100.f)) << "\n\n";
+	std::cout << "\n\n" << upper([&](const SignalBlock x) { return sineGen(x); }, SignalBlock(440.f / 44100.f)) << "\n\n";
 
 
 	// IntegerDelay p(100);
-	//std::cout << "\n\n" << p(DSPVector(), DSPVector()) << "\n" << p(DSPVector(), DSPVector()) << "\n\n";
-	
-//	DSPVector y = lp1(tick);
-	auto lpTestFn = [&](const DSPVector x){ return (x); };
-	
+	//std::cout << "\n\n" << p(SignalBlock(), SignalBlock()) << "\n" << p(SignalBlock(), SignalBlock()) << "\n\n";
+
+//	SignalBlock y = lp1(tick);
+	auto lpTestFn = [&](const SignalBlock x){ return (x); };
+
 
 	FeedbackDelayFunction feedbackFn;
-	
-	// set the delay time. a time < kFloatsPerDSPVector will not work.
-	DSPVector vDelayTime2(65.f);
+
+	// set the delay time. a time < kFramesPerBlock will not work.
+	SignalBlock vDelayTime2(65.f);
 	std::cout << feedbackFn(tick, lpTestFn, vDelayTime2 ) << "\n";
 	for(int i=0; i<4; ++i)
 	{
-		std::cout << feedbackFn(DSPVector(), lpTestFn, vDelayTime2 ) << "\n";
+		std::cout << feedbackFn(SignalBlock(), lpTestFn, vDelayTime2 ) << "\n";
 	}
-	
+
 	std::cout << "\n\n\n\n";
-	
-	DSPVector tick2;
+
+	SignalBlock tick2;
 	tick2[20] = 1; // after PitchbendableDelay warmup
 
-	PitchbendableDelay pd1;	
-	DSPVector vDelayTime3(4.f);
+	PitchbendableDelay pd1;
+	SignalBlock vDelayTime3(4.f);
 	std::cout << pd1(tick2, vDelayTime3) << "\n";
 	for(int i=0; i<4; ++i)
 	{
-		std::cout << pd1(DSPVector(), vDelayTime3) << "\n";
+		std::cout << pd1(SignalBlock(), vDelayTime3) << "\n";
 	}
 	std::cout << pd1(tick2, vDelayTime3) << "\n";
 	std::cout << pd1(tick2, 4.f) << "\n";
-	
-	std::cout << "\n\n\n";
-	
-	auto vHiCoeffs = HiShelf::vcoeffs({{0.25f, 1.f, 1.f}}, {{0.3f, 1.f, 2.f}}); // why two sets of braces? see aaltoverb
-	std::cout << vHiCoeffs;
 
 #ifdef _WINDOWS
 	system("pause");
